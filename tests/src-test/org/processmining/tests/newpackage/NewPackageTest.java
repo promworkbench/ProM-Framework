eddd78609bf4359257c58664b970297ceb264326
bfvdonge <b.f.v.dongen@tue.nl>
2010-12-17 09:50:25 +0000

    added test parts
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/NewPackage/Trunk@171 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ tests/src-test/org/processmining/tests/newpackage/NewPackageTest.java
@@ -0,0 +1,29 @@
+package org.processmining.tests.newpackage;
+import org.junit.Test;
+import org.processmining.contexts.cli.CLI;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import junit.framework.AssertionFailedError;
+
+public class NewPackageTest extends TestCase {
+
+  @Test
+  public void testNewPackage1() throws Throwable {
+    String args[] = new String[] {"-l"};
+    CLI.main(args);
+  }
+
+  @Test
+  public void testNewPackage2() throws Throwable {
+    String testFileRoot = System.getProperty("test.testFileRoot", ".");
+    String args[] = new String[] {"-f", testFileRoot+"/NewPackage_Example.txt"};
+    
+    CLI.main(args);
+  }
+  
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(UmaTest.class);
+  }
+  
+}
