23308063e9a30ccfef3e5ed8825bf8f8bf1a243d
hmwverbeek <h.m.w.verbeek@tue.nl>
2020-03-30 13:27:55 +0000

    Initializes the class path for the ProMCLassLoader. This way, the current project is in the class path, and local java files will be scanned for plugins when using the ProMClassLoader (required for Java 9 and later).
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/ProM-Framework/Trunk@43290 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/framework/util/ProMClassLoader.java
+++ src/org/processmining/framework/util/ProMClassLoader.java
@@ -1,12 +1,37 @@
 package org.processmining.framework.util;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class ProMClassLoader extends URLClassLoader {
 
 	public ProMClassLoader(ClassLoader loader) {
-		super(new URL[] { }, loader);
+		super(getClassPathURLs(), loader);
+//		super(new URL[] { }, loader);
+	}
+	
+	/*
+	 * Returns the URLs of all (absolute) paths as found in the current class path.
+	 */
+	private static URL[] getClassPathURLs() {
+		// Get the current class path.
+		String classpath = System.getProperty("java.class.path");
+		// Get the current path separator.
+		String separator = System.getProperty("path.separator");
+		// Separate the paths in the class path.
+		String[] paths = classpath.split(separator);
+		// Convert every entry into a URL. Assume absolute paths in class path.
+		URL[] urls = new URL[paths.length];
+		for (int i = 0; i < paths.length; i++) {
+			try {
+				urls[i] = new URL("file:///" + paths[i]);
+				System.out.println("[PromClassLoader] found in classpath: " + urls[i]);
+			} catch (MalformedURLException e) {
+				System.err.println("[ProMClassLoader] " + e.getMessage());
+			}
+		}
+		return urls;
 	}
 	
 //	protected Class<?> findClass(String name) throws ClassNotFoundException {
